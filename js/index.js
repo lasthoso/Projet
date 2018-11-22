e8deceefbf008f755d0b11ca795140f7cf26d5a2
Lasthoso <lasthoso@gmail.com>
2018-11-22 12:25:39 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var today = new Date();
+
+var year = document.getElementById('year');
+year.innerHTML = today.getFullYear();
+
+var month = document.getElementById('month');
+var m = new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+month.innerHTML = m[today.getMonth()] + "<br>";
+
+var day = document.getElementById('day');
+day.innerHTML = today.getDate() + "<br>";
