8139e657539f69889d87e3b6ac4b64b93d96691a
Lasthoso <lasthoso@gmail.com>
2018-12-06 17:12:05 +0100

    gmail end

--- js/gmail.js
+++ js/gmail.js
@@ -11,7 +11,20 @@ function switchSport() {
 }
 
 function check() {
-	if(){
-		
-	}
+	if(document.getElementById("lastname").value != ""){
+		if(document.getElementById("firstname").value != ""){
+			if (document.getElementById("altgender").value != "" || document.getElementById("other").checked){
+				if (document.getElementById("sport").value != "" || document.getElementById("ot").checked){
+					if (document.getElementById("message").value != ""){
+						submit();
+					}else
+					alert("Votre message est vide");
+				}else
+				alert("Vous n'avez pas rempli le sport alternatif");
+			}else
+			alert("Vous n'avez pas rempli votre genre");
+		}else
+		alert("Vous n'avez pas rempli votre prenom");
+	}else
+	alert("Vous n'avez pas rempli votre nom")
 }
