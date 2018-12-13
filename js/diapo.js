827253b95a6aee7315eab9e521a84ed58aa17b5e
mxlindadcz <45261253+mxlindadcz@users.noreply.github.com>
2018-12-13 10:45:25 +0100

    pokjhg

---
+++ js/diapo.js
@@ -0,0 +1,19 @@
+var i = 0, j = 1;
+change();
+function change(){
+	var id = setInterval(frame, 5);
+	function frame() {
+		if (document.getElementById("diapoB").style.opacity <= 0) {
+			i++;
+			i %= 14;
+			j++;
+			j %= 14;
+			document.getElementById("diapoB").style.backgroundImage = "url(../media/img/diapo/image" + i + ".jpg)";
+			document.getElementById("diapoB").style.opacity = 1;
+			document.getElementById("diapoA").style.backgroundImage = "url(../media/img/diapo/image" + j + ".jpg)";
+			clearInterval(id);
+		} else {
+			document.getElementById("diapoB").style.opacity -= .01;
+		}
+	}
+}
